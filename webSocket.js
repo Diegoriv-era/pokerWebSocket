6d5d548b4f45777e6bacb89c9f0c0d55926d23dc
Diego Rivera <91293110+Diegoriv-era@users.noreply.github.com>
2024-03-28 19:16:41 -0700

    implemented joining games
    now when a user clicks the join game button it should join them to the specific game session.

--- webSocket.js
+++ webSocket.js
@@ -7,7 +7,8 @@ const cors = require("cors");
 app.use(cors());
 
 const server = http.createServer(app);
-
+let arrayOfRooms = [];
+let usersRooms = [];
 const io = new Server(server, {
     autoConnect: false,
     reconnect: true,
@@ -34,7 +35,7 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
     console.log("USER CONNECTED: ", socket.id);
-
+    socket.emit("room_created", arrayOfRooms);
     socket.on("changeColor", (data) =>{
         //socket.broadcast.emit("receive_message", data.color);
         socket.to(data.roomName).emit("receive_message",data.color);
@@ -44,16 +45,39 @@ io.on("connection", (socket) => {
     });
 
     // Listen for "disconnect" event
+    
     socket.on("disconnect", () => {
         console.log("USER DISCONNECTED: ", socket.id);
+        console.log("before ",usersRooms);
+        usersRooms.forEach((obj,idx) =>{
+            if(obj.id === socket.id){
+                usersRooms.splice(idx, 1);
+                arrayOfRooms.splice(idx,1);
+                socket.broadcast.emit("removeRoom",obj.room);
+                //return;
+            }
+        });
+        console.log("After ",usersRooms);
+        
         // Perform any cleanup or additional handling here
     });
 
     socket.on("createRoom", (roomName) => {
         console.log(`Joined table ${roomName}`);
+        arrayOfRooms.push(roomName);
+        usersRooms.push({
+            id: socket.id,
+            room: roomName
+        });
+        socket.broadcast.emit("room_created", arrayOfRooms);
         socket.join(roomName);
-        socket.broadcast.emit("room_created", roomName);
+        
     });
+    socket.on("playerJoined", (data) =>{
+        socket.join(data.room)
+        console.log(`${data.userName} is joining room: ${data.room}`)
+    })
+
 })
 
 // io.on('changeColor', (data) =>{
